7f4eb9197c6e5be8ebbc1e8ed8080d99e4944d4f
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-07-05 00:50:50 +0600

    Show Products in Home Page

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import AdminRoute from '../components/protecteRoutes/AdminRoute';
 import Home from './home/Home';
 import Login from './user/Login';
 import Register from './user/Register';
+import ProductDetails from './home/ProductDetails';
 import Dashboard from './user/Dashboard';
 import AdminDashboard from './admin/AdminDashboard';
 import CreateCategory from './admin/CreateCategory';
@@ -16,6 +17,7 @@ const Main = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/login" exact component={Login} />
                 <Route path="/register" exact component={Register} />
+                <Route path="/product/:id" exact component={ProductDetails} />
                 <PrivateRoute path="/user/dashboard">
                     <Dashboard />
                 </PrivateRoute>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
 import Layout from '../Layout';
+import Card from './Card';
+import { showError, showSuccess } from '../../utils/messages';
+import { getCategories, getProducts, getProductDetails } from '../../api/apiProduct';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [limit, setLimit] = useState(30);
+    const [order, setOrder] = useState('desc');
+    const [sortBy, setSortBy] = useState('createdAt');
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        getProducts(sortBy, order, limit)
+            .then(response => setProducts(response.data))
+            .catch(err => setError("Failed to load products!"))
+    }, [])
+
     return (
         <Layout title="Home Page" className="container-fluid">
-            <h1>I am homepage</h1>
+            <div style={{ width: "100%" }}>
+                {showError(error, error)}
+                {showSuccess(success, "Added to cart successfully!")}
+            </div>
+            <div className="row">
+                {products && products.map(product => <Card product={product} key={product._id} />)}
+            </div>
         </Layout>
     )
 }
