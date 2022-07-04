ced8298c2df9f45bafd7b671fe294c29246e7c82
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-07-04 22:40:36 +0600

    Login and Register User Part 2

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Layout from '../Layout';
+import { showError, showLoading } from '../../utils/messages';
+import { login } from '../../api/apiAuth';
 
 const Login = () => {
     const [values, setValues] = useState({
@@ -13,23 +16,66 @@ const Login = () => {
 
     const { email, password, loading, error, redirect, disabled } = values;
 
+
+    const handleChange = e => {
+        setValues({
+            ...values,
+            error: false,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setValues({ ...values, error: false, loading: true, disabled: true });
+
+        login({ email, password })
+            .then(response => {
+                setValues({
+                    email: '',
+                    password: '',
+                    success: true,
+                    disabled: false,
+                    loading: false,
+                    redirect: true
+                })
+            })
+            .catch(err => {
+                let errMsg = 'Something went wrong!';
+                if (err.response) {
+                    errMsg = err.response.data;
+                } else {
+                    errMsg = 'Something went wrong!';
+                }
+                setValues({ ...values, error: errMsg, disabled: false, loading: false })
+            })
+    }
+
+
     const signInForm = () => (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-group">
                 <label className="text-muted">Email:</label>
                 <input name='email' type="email" className="form-control"
-                    value={email} required />
+                    value={email} required onChange={handleChange} />
             </div>
             <div className="form-group">
                 <label className="text-muted">Password:</label>
-                <input name="password" type="password" className="form-control"
+                <input name="password" type="password" onChange={handleChange} className="form-control"
                     value={password} required />
             </div>
             <button type="submit" className="btn btn-outline-primary" disabled={disabled}>Login</button>
         </form>
     );
+
+    const redirectUser = () => {
+        if (redirect) return <Redirect to="/" />
+    }
     return (
         <Layout title="Login" className="container col-md-8 offset-md-2">
+            {redirectUser()}
+            {showLoading(loading)}
+            {showError(error, error)}
             <h3>Login Here,</h3>
             <hr />
             {signInForm()}
--- src/components/user/Register.js
+++ src/components/user/Register.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../Layout';
 import { showError, showLoading } from '../../utils/messages';
+import { register } from '../../api/apiAuth';
 
 const Register = () => {
     const [values, setValues] = useState({
@@ -25,7 +27,28 @@ const Register = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        alert(JSON.stringify(values));
+        setValues({ ...values, error: false, loading: true, disabled: true });
+
+        register({ name, email, password })
+            .then(response => {
+                setValues({
+                    name: '',
+                    email: '',
+                    password: '',
+                    success: true,
+                    disabled: false,
+                    loading: false
+                })
+            })
+            .catch(err => {
+                let errMsg = 'Something went wrong!';
+                if (err.response) {
+                    errMsg = err.response.data;
+                } else {
+                    errMsg = 'Something went wrong!';
+                }
+                setValues({ ...values, error: errMsg, disabled: false, loading: false })
+            })
     }
 
     const signUpForm = () => (
@@ -49,8 +72,19 @@ const Register = () => {
         </form>
     );
 
+    const showSuccess = () => {
+        if (success) return (
+            <div className="alert alert-primary">
+                New Account Created. Please <Link to="/login">Login</Link>.
+            </div>
+        )
+    }
+
     return (
         <Layout title="Register" className="container col-md-8 offset-md-2">
+            {showSuccess()}
+            {showLoading(loading)}
+            {showError(error, error)}
             <h3>Register Here,</h3>
             <hr />
             {signUpForm()}
