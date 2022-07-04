798db65cf3ed3889b691db43306bf40a6669bd49
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-07-04 22:24:18 +0600

    Login and Register User Part 1

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import Layout from '../Layout';
 
 const Login = () => {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: false,
+        loading: false,
+        disabled: false,
+        redirect: false
+    });
+
+    const { email, password, loading, error, redirect, disabled } = values;
+
+    const signInForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Email:</label>
+                <input name='email' type="email" className="form-control"
+                    value={email} required />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password:</label>
+                <input name="password" type="password" className="form-control"
+                    value={password} required />
+            </div>
+            <button type="submit" className="btn btn-outline-primary" disabled={disabled}>Login</button>
+        </form>
+    );
     return (
         <Layout title="Login" className="container col-md-8 offset-md-2">
             <h3>Login Here,</h3>
+            <hr />
+            {signInForm()}
             <hr />
         </Layout>
     );
