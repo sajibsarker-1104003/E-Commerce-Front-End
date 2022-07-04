ca72862b5a72b296c88259ee53be82fa48bc0abc
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-07-05 01:11:23 +0600

    Filter Products by Category Part 2

--- src/components/home/CheckBox.js
+++ src/components/home/CheckBox.js
@@ -1,9 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const CheckBox = ({ categories }) => {
+    const [checked, setChecked] = useState([]);
+    const checkedIds = [...checked];
+
+    const handleToggle = id => () => {
+        // return -1 or first index
+        const foundId = checked.indexOf(id);
+        if (foundId === -1) {
+            checkedIds.push(id);
+        } else {
+            checkedIds.splice(foundId, 1);
+        }
+        setChecked(checkedIds);
+    }
+
+    useEffect(() => {
+        alert(JSON.stringify(checked))
+    }, [checked])
+
     return categories.map(category => (
         <li className="list-unstyled" key={category._id}>
             <input
+                onChange={handleToggle(category._id)}
+                value={checked.indexOf(category._id === -1)}
                 type="checkbox"
                 className="form-check-input"
             />
